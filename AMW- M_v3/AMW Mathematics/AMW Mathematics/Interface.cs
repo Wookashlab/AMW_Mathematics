b7a9d0a7c4d97eb51ce1dc0578736283363992be
Wookashlab <lukaszlab1@gmail.com>
2016-04-16 15:00:38 +0200

    Pierwszy wrzut

---
+++ AMW- M_v3/AMW Mathematics/AMW Mathematics/Interface.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AMW_Mathematics
+{
+    public partial class Interface : Form
+    {
+        Liczwyraz l;
+        string wejscie;
+        Klawiatura klawa = new Klawiatura();
+       
+        
+        public Interface()
+        {
+                     
+            InitializeComponent();
+            this.Show();
+            
+
+            
+
+        }
+        
+        private void button13_Click(object sender, EventArgs e)
+        {
+            wejscie = textBox1.Text + "=";
+            l =  new Liczwyraz();
+            listBox1.Items.Add(wejscie + l.liczwyrazenie(wejscie));
+            wejscie = "";
+            textBox1.Clear();
+        }
+
+        private void klaw_MouseDown(object sender, MouseEventArgs e)     // Obsługa użycia przycisków (kliknięcie + przytrzymanie)              #L
+        {
+                
+                var klawisz = (PictureBox)sender;                       //przypisanie wybranego PictureBox-a do zmiennej                        
+                string wartosc = klawisz.Name.ToString();               //przepisanie nazwy PictureBox-a do stringa                             
+                wartosc =  wartosc.Substring(1);                        //pominięcie litery "k" z nazwy (by wartość == symbolowi)              
+                klawa.Click(klawisz,wartosc, textBox1);                 //wywołanie funkcji obsługjącej użycie przycisku                       
+            
+            
+        }
+     
+        private void klaw_MouseEnter(object sender, EventArgs e)        //Obsługa najechania myszką na przyciski                                #L
+        {
+            var klawisz = (PictureBox)sender;                           
+            string wartosc = klawisz.Name.ToString();
+            wartosc = wartosc.Substring(1);
+            klawa.MyszNa(klawisz, wartosc);                             
+        }
+
+        private void klaw_MouseLeave(object sender, EventArgs e)       //Obsługa zabrania myszki z przycisku                                    #L
+        {
+            var klawisz = (PictureBox)sender;
+            string wartosc = klawisz.Name.ToString();
+            wartosc = wartosc.Substring(1);
+            klawa.MyszZa(klawisz, wartosc);
+        }
+
+        private void klawa_MouseUp(object sender, MouseEventArgs e)    //Obsługa puszczenia przycisku (powtóraka klaw_MouseEnter)               #L
+        {
+            var klawisz = (PictureBox)sender;
+            string wartosc = klawisz.Name.ToString();
+            wartosc = wartosc.Substring(1);
+            klawa.MyszNa(klawisz, wartosc);
+        }
+        private void Interface_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ AMW- M_v3/AMW Mathematics/AMW Mathematics/Logo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AMW_Mathematics
+{
+    public partial class Logo : Form
+    {
+        public Logo()
+        {
+            InitializeComponent();
+        }
+
+        int licznik;
+
+        private void Logo_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            licznik++;
+            if (licznik==10)
+            {
+                Close();
+            }
+        }
+    }
+}
