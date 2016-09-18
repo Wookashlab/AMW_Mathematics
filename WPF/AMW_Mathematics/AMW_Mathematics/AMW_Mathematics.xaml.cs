d3b49b45b95e55261ff5668bb8429e6133b69366
Maciej <hein.maciej@gmail.com>
2016-09-18 17:00:58 +0200

    symbolics

---
+++ WPF/AMW_Mathematics/AMW_Mathematics/AMW_Mathematics.xaml.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MaximaSharp;
+namespace AMW_Mathematics
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Dictionary<string, string> SymbolsAndValues;
+        public MainWindow()
+        {
+            InitializeComponent();
+            SymbolsAndValues = new Dictionary<string, string>(); 
+        }
+        private void ConfirmExpresion_Click(object sender, RoutedEventArgs e)
+        {
+            string Expresion = ExpressionField.Text;
+            Expresion = SaveValuesOfVariables(Expresion);
+            Expresion = CheckVariablesinExpresion(Expresion);
+            if(Expresion.Contains(":=")==false) Expresion = AddToNumberDot(Expresion);
+            Expresion = Maxima.Eval(Expresion);
+            Expresion = Expresion.Replace(":=", " = ");
+            ResultList.Items.Add(Expresion);
+            ExpressionField.Clear();
+        }
+        private string SaveValuesOfVariables(string Expresion)
+        {
+            if (Expresion.Contains(":=") == true) //sprawdzenie wyrażenie ma stworzyć zmienna
+            {
+                Expresion = AddToNumberDot(ExpressionField.Text.Substring(Math.Min(ExpressionField.Text.Length, ExpressionField.Text.IndexOf(":")) + 2));
+                Expresion = Maxima.Eval(Expresion);
+                SymbolsAndValues.Add(ExpressionField.Text.Substring(0, Math.Min(ExpressionField.Text.Length, ExpressionField.Text.IndexOf(":"))), Expresion);
+                return ExpressionField.Text.Substring(0, Math.Min(ExpressionField.Text.Length, ExpressionField.Text.IndexOf(":"))) + ":=" + Expresion;
+            }
+            else return Expresion;
+        }
+        private string AddToNumberDot(string Expresion)
+        {
+           string pom = "";
+           bool pointexist = false;
+           for(int i = 0; i < Expresion.Length; i++)
+           {
+               if(Char.IsNumber(Expresion[i]) == true)
+               {
+                   for(int j = i; j < Expresion.Length; j++)
+                   {
+                        if (Char.IsNumber(Expresion[j]) != true && Expresion[j] != '.')
+                        {
+                            if (pom.Length == 1)
+                            {
+                                Expresion = Expresion.Insert(j, ".0");
+                                i = i + 2;
+                                pom = "";
+                                break;
+                            }
+                            else
+                            {
+                                if (pom.Contains(".")!= true)
+                                {
+                                    Expresion = Expresion.Insert(j, ".0");
+                                    pom = "";
+                                    i = j+2;
+                                    break;
+                                }
+                                else
+                                {
+                                    pom = "";
+                                    i = j;
+                                    break;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            pom = pom + Expresion[j];
+                        }           
+                        if (i + 1 == Expresion.Length)
+                        {
+                            if (pom.Length == 1)
+                            {
+                                Expresion = Expresion.Insert(j+1, ".0");
+                                i = i + 2;
+                                pom = "";
+                                break;
+                            }
+                            else
+                            {
+                                if (pom.Contains(".") != true)
+                                {
+                                    Expresion = Expresion.Insert(j+1, ".0");
+                                    pom = "";
+                                    i = j + 2;
+                                    break;
+                                }
+                                else
+                                {
+                                    pom = "";
+                                    i = j;
+                                    break;
+                                }
+                            }
+                        }
+                   }
+               }
+           }
+           return Expresion;
+        }
+        private string CheckVariablesinExpresion(string Expresion)
+        {
+            if (Expresion.Contains(":=") != true)
+            {
+                foreach (var symbol in SymbolsAndValues)
+                {
+                    Expresion = Expresion.Replace(symbol.Key, symbol.Value);
+                }
+            }
+            return Expresion;
+        }
+    }
+}
