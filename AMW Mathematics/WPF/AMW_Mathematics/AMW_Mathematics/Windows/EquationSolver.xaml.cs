156b4c65f091d45deee464804f3759d6cc89f131
Maciej <hein.maciej@gmail.com>
2016-10-30 19:37:14 +0100

    komentarze

--- AMW Mathematics/WPF/AMW_Mathematics/AMW_Mathematics/Windows/EquationSolver.xaml.cs
+++ AMW Mathematics/WPF/AMW_Mathematics/AMW_Mathematics/Windows/EquationSolver.xaml.cs
@@ -129,92 +129,92 @@ namespace AMW_Mathematics.Windows
                 }
                 else                                                                                                    //w przypadku gdy rzutowanie zmiennej powiodło się w zmiennej c znajduje się obiekt klasy ExpresionsToSolve dzięki czemu możemy odowłać się do jego atrybutów #M
                 {
-                    if(c.Exp != "")
+                    if(c.Exp != "")                                                                                     //dodanie do listy obliczanych elementów równaniń tylko tych kontenerów, w których znajduje się jakieś równania dzięki temu zabezpieczamy możliwość zostawienia pustego konteneru przez użytkownika #M
                     {
-                        variable = ExpresionFindVariable(c.Exp, variable);
-                        lisetexpresiontosolve.Add(new ExpresionsToSolve { expresion = c.Exp, variable = variable });
+                        variable = ExpresionFindVariable(c.Exp, variable);                                              //funcja zapisuje do zmiennej wszystkie niewiadome jakie wystąpiły w elemencie równania #M
+                        lisetexpresiontosolve.Add(new ExpresionsToSolve { expresion = c.Exp, variable = variable });    //dodanie do listy elemetu równania wraz z jego niewiadomymi #M
                     }                   
                 }
             }
-            MessageBox.Show(solve(lisetexpresiontosolve, ""));
+            MessageBox.Show(solve(lisetexpresiontosolve, ""));                                                          //wywołanie funkcji solve której parametrem jest lista elementów równania wraz z niewiadomymi efektem wykonania funkcji jest zwrócenie do zmiennej typu string rozwiązania równania #M                                                   
         }
         private string solve(List<ExpresionsToSolve> lisetexpresiontosolve, string expres)
         {
-            try
+            try 
             {
-                bool goodexpresioncheck = true;
-                var list = lisetexpresiontosolve.Select(m => m.variable);
-                var c = list.Aggregate("", (max, cur) => max.Length > cur.Length ? max : cur).ToString();
-                int index = list.ToList().FindIndex(m => m.Contains(c.ToString()));
-                foreach (var item in list.ToList()) //sprawdzenie czy rowanie zostalo doprze zdeklarowane poprzez sprawdzenie czy rówanie nie zawiera parametrów kórych nie ma
-                {
-                    for (int i = 0; i < item.ToString().Length; i++)
+                bool goodexpresioncheck = true;                                                                                 //deklaracja zmiennej dzięki której jesteśmy w stanie stwierdzić czy równanie został dobrze sformułowane przez użytkownika #M
+                var list = lisetexpresiontosolve.Select(m => m.variable);                                                       //przy użyciu LINQ przypisanie do zmiennej typu object listy zmiennych występujacych w poszczególnych elementach równania #M 
+                var c = list.Aggregate("", (max, cur) => max.Length > cur.Length ? max : cur).ToString();                       //przypisanie do zmiennej typu object ciągu wszystich zmiennych które wystąpiły w równaniu #M 
+                int index = list.ToList().FindIndex(m => m.Contains(c.ToString()));                                             //przypisanie do zmiennej index pozycli w liście tego elementu równania w którym wystąpiły wszystkie zmienne #M
+                foreach (var item in list.ToList())                                                                             //pętla należy do sprawdzenia czy rowanie zostalo dobrze zdeklarowane poprzez sprawdzenie czy w elementach równania nie występują źle zdeklarowane niewiadomoe #M
+                {                                                                                                               //przeszukanie całej listy elementów równania i sprawdzenie czy nie występują w nim niewiadome których nie ma #M
+                    for (int i = 0; i < item.ToString().Length; i++)                                                            //pętla sprawdzająca czy w elemencie równania nie znajdują się niezdeklarowane zmienne #M
                     {
-                        if (c.ToString().Contains(item.ToString()[i]) != true)
+                        if (c.ToString().Contains(item.ToString()[i]) != true)                                                  //gdy warunek nie jest spełniony oznacza to że element równania zawiera niezdeklarowane zmienne #M
                         {
-                            goodexpresioncheck = false;
+                            goodexpresioncheck = false;                                                                         //ustawenie zmiennej na false powoduje nieobliczanie wartości równia #M
                             break;
                         }
-                        if (i == item.ToString().Length - 1 && list.ToList().Count() != c.ToString().Replace(",", "").Length)
+                        if (i == item.ToString().Length - 1 && list.ToList().Count() != c.ToString().Replace(",", "").Length)   //sprawdzenie czy liczba niewiadomych jest równa ilości równań jeśli tak nie jest oznacza to że równanie jest źle sformułowane #M 
                         {
-                            goodexpresioncheck = false;
+                            goodexpresioncheck = false;                                                                         //ustawenie zmiennej na false powoduje nieobliczanie wartości równia #M            
                             break;
                         }
                     }
                 }
-                if (goodexpresioncheck == true)
+                if (goodexpresioncheck == true)                                                                                 //sprawdzenie czy walidacja równania przebiegła prawidołow #M
                 {
-                    if (lisetexpresiontosolve.Count > 1)
+                    if (lisetexpresiontosolve.Count > 1)                                                                        //Obliczanie równania w przypadku gdy równania składa się z więcej niż jednego elemetu #M
                     {
                         foreach (var item in lisetexpresiontosolve)
+                        {
+                            expres = expres + "," + item.expresion;                                                             //przypisanie do zmiennej expres wszystkich elementów równania w celu przygotowania ich do wprowadzania do Maximi #M
+                        }
+                        expres = expres.Remove(0, 1);                                                                           //usunięcie perwszego znaku który był przecinkiem #M
+                        expres = "linsolve([" + expres + "],[" + lisetexpresiontosolve[index].variable + "])";                  //przygotowanie równania do wprowadzenia do maximy #M
+                        expres = Maxima.Eval(expres);                                                                           //obliczenie wartości równania przez maxime #M
+                        int ind = expres.IndexOf('[');
+                        expres = expres.Remove(0, ind);
+                        return expres;                                                                                          //zwrócenie obliczonych niewiadomych w postaci stringa #M
+                    }
+                    if (lisetexpresiontosolve.Count == 1)                                                                       //w przypadku gdy równanie składa się z jednego elementu #M
+                    {
+                        foreach (var item in lisetexpresiontosolve)                                     
                         {
                             expres = expres + "," + item.expresion;
                         }
                         expres = expres.Remove(0, 1);
-                        expres = "linsolve([" + expres + "],[" + lisetexpresiontosolve[index].variable + "])";
+                        expres = "solve(" + expres + "," + lisetexpresiontosolve[index].variable + ")";                         //wszystko przebiega tak jak w przypadku równań z wiekszą ilością elementów poza faktem, że do maximy wprowadzamy inny rodzaj funkcji #M
                         expres = Maxima.Eval(expres);
                         int ind = expres.IndexOf('[');
                         expres = expres.Remove(0, ind);
-                        return expres;
+                        return expres;                                                                                          //zwrócenie obliczonych niewiadomych w postaci stringa #M
                     }
-                    if (lisetexpresiontosolve.Count == 1)
-                    {
-                        foreach (var item in lisetexpresiontosolve)
-                        {
-                            expres = expres + "," + item.expresion;
-                        }
-                        expres = expres.Remove(0, 1);
-                        expres = "solve(" + expres + "," + lisetexpresiontosolve[index].variable + ")";
-                        expres = Maxima.Eval(expres);
-                        int ind = expres.IndexOf('[');
-                        expres = expres.Remove(0, ind);
-                        return expres;
-                    }
-                    return "Mistake in Equation";
+                    return "Mistake in Equation";                                                                               //błąd w obliczani będne równanie zwraca komunikat błędu w postaci stringa #M
                 }
                 else
                 {
-                    return "Mistake in Equation";
+                    return "Mistake in Equation";                                                                               //błąd w obliczani będne równanie zwraca komunikat błędu w postaci stringa #M
                 }
             }
             catch
             {
-                return "Mistake in Equation";
+                return "Mistake in Equation";                                                                                   //błąd w obliczani będne równanie zwraca komunikat błędu w postaci stringa #M
             }
         }
-        private string ExpresionFindVariable(string expresion, string variable)
+        private string ExpresionFindVariable(string expresion, string variable)                                                 //funcka odpowiadająca za stowrzenie ciągu niewiadomych występujących w elemencie równania #M
         {
             try
             {
-                foreach (var item in VariableList)
+                foreach (var item in VariableList)                                                                              //pętla wprowadzająca do zmiennej typu string niewiadomych występujących w elemencie równania #M                                                                                       
                 {
-                    if (expresion.Contains(item) == true)
+                    if (expresion.Contains(item) == true)                                                                       //Sprawdzenie cz0y w elemenie równania znajduje się niewiadoma z listy #M
                     {
-                        variable = variable + "," + item;
+                        variable = variable + "," + item;                                                                       //w przypadku gdy niewiadoma znajduje się w elemecie równania dodanie jej do ciągu niewiadomych
                     }
                 }
-                variable = variable.Remove(0, 1);
-                return variable;
+                variable = variable.Remove(0, 1);                                                                               //usunięcie niepotrzebnych znaków #M
+                return variable;                                                                                                //zwrócenie ciągu niewiadomych #M                                                                               
             }
             catch
             {
@@ -223,9 +223,9 @@ namespace AMW_Mathematics.Windows
            
         }
     }
-    public class ExpresionsToSolve
+    public class ExpresionsToSolve                                                                                          //klasa odpowiadająca za stworzenie listy elementów rowniania wraz z występującymi w nich niewiadomymi #M
     {
-        public string expresion { get; set; }
-        public string variable { get; set; }
+        public string expresion { get; set; }                                                                               //element równania #M
+        public string variable { get; set; }                                                                                //niewiadoma #M
     }
 }
