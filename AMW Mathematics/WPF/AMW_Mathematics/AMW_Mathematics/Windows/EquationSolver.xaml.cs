8cd6346fdef65c12d657a50210219da79228de42
Maciej <hein.maciej@gmail.com>
2016-10-30 08:37:02 +0100

    drobne poprawki

--- AMW Mathematics/WPF/AMW_Mathematics/AMW_Mathematics/Windows/EquationSolver.xaml.cs
+++ AMW Mathematics/WPF/AMW_Mathematics/AMW_Mathematics/Windows/EquationSolver.xaml.cs
@@ -24,10 +24,28 @@ namespace AMW_Mathematics.Windows
         public EquationSolver()
         {
             InitializeComponent();
-            DataContext = new ComboListEquationView();
             ListViewExp.Items.Add(new List<ListExpresionView> {
                                  new ListExpresionView { Exp = "", Watermark = "Equation 1" }
             });
+        }
+
+        private void CoEquations_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                int index = CoEquations.SelectedIndex;
+                int count;
+                if (ListViewExp.Items.Count <= index && ListViewExp.Items.Count < 6)
+                {
+                    count = index - ListViewExp.Items.Count +1;
+                    for (int i = 1; i <= count; i++)
+                    {
+                        int val = ListViewExp.Items.Count + 1;
+                        ListViewExp.Items.Add(new ListExpresionView { Exp = "", Watermark = "Equation " +  val});
+                    }
+                }
+            }
+            catch { }
         }
     }
 }
